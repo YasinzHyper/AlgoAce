c1e98ae8746f4a3cb9ced711afca4c2e2d0e5a5b
Mohammed Yasin Zuhayr <113431400+YasinzHyper@users.noreply.github.com>
2025-05-28 00:15:19 +0530

    Homepage tweaks: Added testimonial dynamic component

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -10,7 +10,7 @@ import {
 } from "@/components/ui/sidebar";
 import { AppSidebar } from "@/components/sidebar/app-sidebar";
 import { ThemeProvider } from "@/components/theme-provider";
-import { AuthProvider } from "@/contexts/auth-context"
+import { AuthProvider } from "@/contexts/auth-context";
 // import { ThemeProvider } from "@/components/theme-provider"
 
 const inter = Inter({ subsets: ["latin"] });
@@ -29,23 +29,23 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
         <AuthProvider>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <SidebarProvider>
-            <AppSidebar />
-            <SidebarInset>
-              <header className="flex h-14 items-center gap-4 border-b bg-background px-4 lg:h-16">
-                <SidebarTrigger className="cursor-pointer" />
-                <div className="flex-1" />
-              </header>
-              <main className="flex-1 p-4 md:p-6">{children}</main>
-            </SidebarInset>
-          </SidebarProvider>
-        </ThemeProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <SidebarProvider>
+              <AppSidebar />
+              <SidebarInset style={{overflowX:"hidden"}}>
+                <header className="flex h-14 items-center gap-4 border-b bg-background px-4 lg:h-16">
+                  <SidebarTrigger className="cursor-pointer" />
+                  <div className="flex-1" />
+                </header>
+                <main className="flex-1 p-4 md:p-6" style={{overflowX:"hidden"}}>{children}</main>
+              </SidebarInset>
+            </SidebarProvider>
+          </ThemeProvider>
         </AuthProvider>
       </body>
     </html>
